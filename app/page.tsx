87b4fa9ad29d985a13f3c8e583a2f83e3431955f
Nayanchandrakar <chandrakarnayan96@gmail.com>
2025-08-12 13:34:31 +0530

    added: simple text input to check it

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,76 @@
+"use client"
+
+import { Input } from "@/components/ui/input"
+import { CircleCheck, Info, Loader } from "lucide-react"
+import { useEffect, useState } from "react"
+import { useDebounceValue } from "usehooks-ts"
+import { checkUserName } from "@/app/actions/username-actions"
+import { cn } from "@/lib/utils"
+
 export default function HomePage() {
-  return <div className="">Home page</div>
+  const [isTaken, setIsTaken] = useState(false)
+  const [isPending, setIsPending] = useState(false)
+  const [userName, setUserName] = useDebounceValue("", 300)
+
+  const onSubmit = async () => {
+    if (!userName.length) return
+
+    try {
+      setIsPending(true)
+      const response = await checkUserName({ username: userName })
+      setIsTaken(response)
+    } finally {
+      setIsPending(false)
+    }
+  }
+
+  useEffect(() => {
+    onSubmit()
+  }, [userName])
+
+  return (
+    <section className="w-full min-h-screen flex items-center justify-center">
+      <div>
+        <div className="flex items-start flex-col gap-3">
+          <label className="text-sm leading-none font-medium">Username</label>
+          <div className="flex flex-col gap-2">
+            <Input
+              type="text"
+              onChange={(event) => setUserName(event.target.value)}
+              placeholder="enter your username here..."
+              className=""
+            />
+            {!!userName.length ? (
+              <span
+                className={cn(
+                  " text-xs flex gap-1 items-center",
+                  isPending && userName
+                    ? "text-yellow-500"
+                    : isTaken
+                    ? "text-red-500"
+                    : "text-green-500"
+                )}
+              >
+                {isPending ? (
+                  <Loader className="size-3 animate-spin" />
+                ) : isTaken ? (
+                  <CircleCheck className="size-3" />
+                ) : (
+                  <Info className="size-3" />
+                )}
+
+                {isPending
+                  ? "Checking availablility..."
+                  : isTaken
+                  ? "username already in use."
+                  : "username is available"}
+              </span>
+            ) : (
+              <span className="h-3.5" />
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
